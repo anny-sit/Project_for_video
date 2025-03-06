515b925c411430661c3508625ab0abf919c0c852
anny-sit <annysit@yandex.ru>
2025-03-06 15:18:23 +0300

    hw commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,26 @@ import java.util.Arrays;
 //Возвращаем кусок списка с 3-го по 7-й элемент.
 
 public class Main {
+    public static void main(String[] args) {
 
+        int[] myArray = {10, 13, 8, 4, 3, 19, 32, 43, 65};
+        int[] array = myMethod(myArray);
+        Arrays.stream(array)
+                .forEach(System.out::println);
+
+    }
+
+    public static int[] myMethod(int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                numbers[i] = numbers[i] + 1;
+            }
+        }
+
+        return Arrays.stream(numbers)
+                .skip(2)
+                .limit(5)
+                .toArray();
+    }
 }
